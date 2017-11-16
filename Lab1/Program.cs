d7684409a500d39aadef44c4c2a18a3e74fdaeb0
Almaz Murzin <almaz.murzin.gdc@ts.fujitsu.com>
2017-11-16 17:39:21 +0300

    initial

---
+++ Lab1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static public double funcEuler(double x, double y)
+        {
+            return (Math.Pow(Math.Cos(x), 2.0) + y * Math.Tan(x));
+        }
+        static public double funcExact(double x)
+        {
+            double C = -(Math.Pow(2.0, 0.5) / 6.0);
+            return (C / Math.Cos(x) + 3.0 / 4.0 * Math.Tan(x) + 1.0 / 12.0 * (Math.Sin(3.0 * x) / Math.Cos(x)));
+        }
+        static void Main(string[] args)
+        {
+            double h = 0.05;
+            double x0 = Math.PI / 4.0;
+            double y0 = 0.5;
+            const double xLimit = Math.PI / 2.0;           
+            double x = x0;
+            double y = y0;
+            double f;
+            double hf;
+            double exactY;
+            Console.WriteLine("Шаг = 0.1");     
+            while (x<=xLimit)
+            {
+                f = funcEuler(x, y);
+                hf = h * f;
+                exactY = funcExact(x);
+                Console.WriteLine(String.Format("x = {0:0.###}, y = {1:0.###}, yExact = {4:0.###}, delta = {5:0.###}, f(x,y) = {2:0.###}, hf(x,y) = {3:0.###}", x, y, f, hf, exactY, exactY - y));
+                x = x + h;
+                y = y + hf;
+            }
+            h = 0.1;
+            x = x0;
+            y = y0;
+            Console.WriteLine("Шаг = 0.2");
+            while (x <= xLimit)
+            {
+                f = funcEuler(x, y);
+                hf = h * f;
+                exactY = funcExact(x);
+                Console.WriteLine(String.Format("x = {0:0.###}, y = {1:0.###}, yExact = {4:0.###}, delta = {5:0.###}, f(x,y) = {2:0.###}, hf(x,y) = {3:0.###}", x, y, f, hf, exactY, exactY - y));
+                x = x + h;
+                y = y + hf;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        private double step;
+        private const double piecesCount = 100;
+        private const double minX = -1 * Math.PI;
+        private const double maxX = Math.PI;
+        private const int furieStepCount = 10;
+        private void SetupChart()
+        {
+            this.chart.Series.Clear();
+            this.chart.Titles.Add("Графики функций");
+            this.chart.ChartAreas[0].AxisX.LabelStyle.Format = "{0:#.###}";
+        }
+        private void DrawFunctions()
+        {
+            double x = minX;
+            Series commonSeries = this.chart.Series.Add("График основной функции");
+            commonSeries.ChartType = SeriesChartType.Line;
+            Series furieSeries = this.chart.Series.Add("График суммы ряда Фурье");
+            furieSeries.ChartType = SeriesChartType.Line;
+            while (x<maxX)
+            {
+                commonSeries.Points.AddXY(x, CommonFunction(x));
+                furieSeries.Points.AddXY(x, FurieFunction(x));
+                x += step;
+            }
+            
+        }
+        private double CommonFunction(double x)
+        {
+            double ans = 0;
+            if (x >= minX && x < 0) ans = 0;
+            else if (x >= 0 && x <= maxX) ans = 1 - 4 * x;
+            return ans;
+        }
+        private double FurieFunction(double x)
+        {
+            double a0 = 1 - 2 * Math.PI;
+            double sum = 0; 
+            for (int n=1;n<=furieStepCount;n++)
+            {
+                sum += ((4*(1 - Math.Pow(-1, n))) / (n * n)) * Math.Cos(n * x);
+                sum += (((4 * Math.PI - 1) * Math.Pow(-1, n) + 1 ) / n) * Math.Sin(n * x);
+            }
+            sum /= Math.PI;
+            sum += a0/2;
+            return sum;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            SetupChart();
+            step = (maxX - minX) / piecesCount;
+            DrawFunctions();
+        }
+
+        private void chart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
